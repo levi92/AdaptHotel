e15e6dd5c02e855fb3af26c041b9265037613bc3
GabrieleGoncalves <gabriele.goncalves@outlook.com.br>
2016-05-13 01:51:47 -0300

    Classes
    Classes

---
+++ AdaptHotel/AdaptHotel/App_Code/Classe/Administrador.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class Administrador : Pessoa 
+{
+    private int codAdm;
+
+
+    public Administrador(string nome, string telefone, string email, string cpf, string senha, char sexo, DateTime dataNascimento, int codPessoa, int codAdm, Endereco end)
+        : base(nome, telefone, email, cpf, sexo, dataNascimento, codPessoa, end, senha)
+	{
+        this.codAdm = codAdm;
+	}
+
+    public int CodAdm
+    {
+        get { return codAdm; }
+        set { codAdm = value; }
+    }
+
+    public string ImprimirAdministrador()
+    {
+        return base.ImprimirPessoa() ;
+    }
+
+}
