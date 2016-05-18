30b09e9e11e4b7310f9ffbc582e8fc2b5dbf37f8
Nicolas Santos <nicolas@hexatae.com>
2016-05-18 14:57:42 -0300

    Subindo classes novamente

---
+++ AdaptHotel/AdaptHotel/App_Code/Classes/Avaliacoes.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Avaliacoes
+/// </summary>
+public class Avaliacoes
+{
+    private string comentario, resposta;
+    private int codAvaliacoes, nota;
+    private DateTime dataHoraResposta, dataHoraComentario;
+    private Hospede hos;
+    private SubTipoQuarto subQ;
+
+    public Avaliacoes(string comentario, string resposta, int codAvaliacoes, int nota, DateTime dataHoraResposta, DateTime dataHoraComentario, Hospede hos, SubTipoQuarto subQ)
+    {
+        this.Comentario = comentario;
+        this.Resposta = resposta;
+        this.CodAvaliacoes = codAvaliacoes;
+        this.Nota = nota;
+        this.DataHoraResposta = dataHoraResposta;
+        this.DataHoraComentario = dataHoraComentario;
+        this.Hos = hos;
+        this.subQ = subQ;
+    }
+
+    public SubTipoQuarto SubQ
+    {
+        get { return subQ; }
+        set { subQ = value; }
+    }
+
+        public string Comentario
+    {
+        get
+        {
+            return comentario;
+        }
+
+        set
+        {
+            comentario = value;
+        }
+    }
+
+    public string Resposta
+    {
+        get
+        {
+            return resposta;
+        }
+
+        set
+        {
+            resposta = value;
+        }
+    }
+
+    public int CodAvaliacoes
+    {
+        get
+        {
+            return codAvaliacoes;
+        }
+
+        set
+        {
+            codAvaliacoes = value;
+        }
+    }
+
+    public int Nota
+    {
+        get
+        {
+            return nota;
+        }
+
+        set
+        {
+            nota = value;
+        }
+    }
+
+    public DateTime DataHoraResposta
+    {
+        get
+        {
+            return dataHoraResposta;
+        }
+
+        set
+        {
+            dataHoraResposta = value;
+        }
+    }
+
+    public DateTime DataHoraComentario
+    {
+        get
+        {
+            return dataHoraComentario;
+        }
+
+        set
+        {
+            dataHoraComentario = value;
+        }
+    }
+
+    public Hospede Hos
+    {
+        get
+        {
+            return hos;
+        }
+
+        set
+        {
+            hos = value;
+        }
+    }
+
+    public string ImprimirAvaliacoes()
+    {
+        return Hos.ImprimirHospede()+"COMENTARIO: "+Comentario+ "DATA E HORA COMENTARIO: "+ DataHoraComentario+"RESPOSTA: "+Resposta+ "DATA E HORA RESPOSTA: "+DataHoraResposta+"NOTA: "+Nota+ "HOSPEDE: "+Hos+ "SUB TIPO DE QUARTO: "+SubQ; // ver um nome melhor para subtipodequarto
+    }
+}
---
+++ AdaptHotel/AdaptHotel/App_Code/Classes/DescontoQuarto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class DescontoQuarto
+{
+    private double valor;
+    private DateTime dataInicio, dataFim;
+    private SubTipoQuarto subTipoQuarto;
+
+	public DescontoQuarto(double valor, DateTime dataInicio, DateTime dataFim, SubTipoQuarto subTipoQuarto)
+	{
+        this.valor = valor;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        this.subTipoQuarto = subTipoQuarto;
+	}
+
+    public SubTipoQuarto SubTipoQuarto
+    {
+        get { return subTipoQuarto; }
+        set { subTipoQuarto = value; }
+    }
+
+    public DateTime DataFim
+    {
+        get { return dataFim; }
+        set { dataFim = value; }
+    }
+
+    public DateTime DataInicio
+    {
+        get { return dataInicio; }
+        set { dataInicio = value; }
+    }
+
+    public double Valor
+    {
+        get { return valor; }
+        set { valor = value; }
+    }
+
+    public string ImprimirDescontoQuarto()
+    {
+        return "VALOR: " + Valor + "DATA INICIAL: " + DataInicio + "DATA FINAL: " + DataFim + "SUB TIPO DE QUARTO: " + SubTipoQuarto;
+    }
+
+}
