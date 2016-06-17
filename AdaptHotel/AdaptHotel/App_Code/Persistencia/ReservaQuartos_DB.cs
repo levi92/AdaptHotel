56352d2a018078c47c3267ec197fae01060583d7
GabrieleGoncalves <gabriele.goncalves@outlook.com.br>
2016-06-16 23:59:06 -0300

    Persistencias com erros

---
+++ AdaptHotel/AdaptHotel/App_Code/Persistencia/ReservaQuartos_DB.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for reservaQuartos_DB
+/// </summary>
+public class reservaQuartos_DB
+{
+    public static void Insert()
+    {
+        SqlConnection objConnection;
+        SqlCommand objCommand;
+        SqlDataAdapter objDataAdapter;
+        objConnection = Mapped.Connection();
+       // objCommand = Mapped.Command("Insert into avaliacoes (codReservaQuartos, codReserva, codNumeroQuarto) values @codReservaQuartos, @codReserva, @codNumeroQuarto");
+        objDataAdapter = Mapped.Adapter(objCommand);
+        objConnection.Close();
+        objCommand.Dispose();
+        objConnection.Dispose();
+
+    }
+
+}
